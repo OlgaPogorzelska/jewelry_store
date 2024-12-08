13ba3c4c7f132abfbfc707f28179120ad3c165bd
Olga Pogorzelska <pogorzelska0496@gmail.com>
2024-12-08 21:58:00 +0100

    Add base page with basic layout

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class StartView(View):
+    """
+        Main View
+    """
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'base.html')
