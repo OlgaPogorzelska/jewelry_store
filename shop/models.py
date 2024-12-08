5cc4fa99f24dc81325e67cfc3acd1143e7a34933
Olga Pogorzelska <pogorzelska0496@gmail.com>
2024-12-08 21:32:52 +0100

    Add CustomerUser model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomerUser(AbstractUser):
+    """
+        Custom user model for a jewelry store.
+    """
+    phone_number = models.CharField(max_length=9, unique=True, null=True)
+    street = models.CharField(max_length=255, null=True)
+    house_number = models.CharField(max_length=10, null=True)
+    apartment_number = models.CharField(max_length=10, blank=True, null=True)
+    city = models.CharField(max_length=100, null=True)
+    postal_code = models.CharField(max_length=20, null=True)
+    country = models.CharField(max_length=100, null=True, default="Poland")
+
+    @property
+    def full_street(self):
+        if self.apartment_number:
+            return f"{self.street}  {self.house_number} {self.apartment_number}"
+        return f"{self.street}  {self.house_number}"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
