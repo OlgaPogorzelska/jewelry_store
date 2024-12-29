a4ef76f6034134ba77306dd0d96caa8567cdb83f
Olga Pogorzelska <pogorzelska0496@gmail.com>
2024-12-29 14:30:38 +0100

    Adding validation from form to model

--- shop/forms.py
+++ shop/forms.py
@@ -25,75 +25,7 @@ class RegistrationUserForm(forms.ModelForm):
         pwd2 = cd.get('confirm_password')
         if pwd1 and pwd2 and pwd1 != pwd2:
             self.add_error('confirm_password', 'The passwords are not the same')
-
-        p_code = cd.get('postal_code')
-        if p_code and not is_valid_postal_code(p_code):
-            self.add_error('postal_code', '''Postal code must be in the
-                                  format 'XX-XXX'. ''')
-
-        f_name = cd.get('first_name')
-        l_name = cd.get('last_name')
-        street = cd.get('street')
-        house_num = cd.get('house_number')
-        apartment_num = cd.get('apartment_number')
-        city = cd.get('city')
-        country = cd.get('country')
-        phone_number = cd.get('phone_number')
-
-        if f_name and not f_name.isalpha():
-            self.add_error('first_name', 'First name must contain only letters.')
-        if l_name and not l_name.isalpha():
-            self.add_error('last_name', 'Last name must contain only letters.')
-        if phone_number and not phone_number.isdigit():
-            self.add_error('phone_number', 'Phone number must contain only digits.')
-        if street and not validate_street_name(street):
-            self.add_error('street', '''Street name must contain only letters,
-                                  spaces, and optional numbers at the end. 
-                                  Example: "Jana Pawła II", "Dywizjonu 303".''')
-        if house_num and not validate_house_number(house_num):
-            self.add_error('house_number', '''House number must be a number
-                                  with an optional letter at the end.
-                                  Examples: "123", "456A".''')
-        if apartment_num and apartment_num and not apartment_num.isdigit():
-            self.add_error('apartment_number', 'Apartment number must contain only number.')
-        if city and not city.isalpha():
-            self.add_error('city', 'City name must contain only letters.')
-        if country and not country.isalpha():
-            self.add_error('country', 'Country name must contain only letters. ')
         return cd
-
-
-@staticmethod
-def is_valid_postal_code(postal_code):
-    """
-        Checks if the postal code is written in
-         the form XX-XXX where X is a number.
-    """
-    return (len(postal_code) == 6
-            and postal_code[:2].isdigit()
-            and postal_code[2] == '-'
-            and postal_code[3:].isdigit())
-
-
-@staticmethod
-def validate_house_number(house_number):
-    """
-        Checks if house_number is a number
-         with an optional trailing letter.
-    """
-    pattern = r'^\d+[A-Za-z]?$'  # liczba z opcjonalną literą
-    return re.match(pattern, house_number)
-
-
-@staticmethod
-def validate_street_name(street):
-    """
-        Checks if street is word or words with space and
-         with an optional trailing number.
-    """
-    pattern = r'[A-Za-zĄąĆćĘęŁłŃńÓóŚśŹźŻż ]+(\d+)?$'  # słowo lub
-    # słowa ze spacja pomiędzy sobą i z możliwością liczby na końcu
-    return re.match(pattern, street)
 
 
 class UserLoginForm(forms.Form):
--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,71 @@
-import uuid
-
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.core.exceptions import ValidationError
+import re
 from django.db import models
+
+
+def validate_postal_code(postal_code):
+    """
+        Checks if the postal code is written in
+         the form XX-XXX where X is a number.
+    """
+    if (len(postal_code) == 6 and postal_code[:2].isdigit()
+            and postal_code[2] == '-' and postal_code[3:].isdigit()):
+        return True
+    else:
+        raise ValidationError("Postal code must be in the format 'XX-XXX'.")
+
+
+def validate_house_number(house_number):
+    """
+        Checks if house_number is a number
+         with an optional trailing letter.
+    """
+    pattern = r'^\d+[A-Za-z]?$'  # liczba z opcjonalną literą
+    if re.match(pattern, house_number):
+        return True
+    else:
+        raise ValidationError("House number must be a number with an "
+                              "optional letter at the end. "
+                              "Examples: '123', '456A'")
+
+
+def validate_street_name(street):
+    """
+        Checks if street is word or words with space and
+         with an optional trailing number.
+    """
+    pattern = r'[A-Za-zĄąĆćĘęŁłŃńÓóŚśŹźŻż ]+(\d+)?$'  # słowo lub
+    # słowa ze spacja pomiędzy sobą i z możliwością liczby na końcu
+    if re.match(pattern, street):
+        return True
+    else:
+        raise ValidationError("Street name must contain only letters, "
+                              "spaces, and optional numbers at the end. "
+                              "Examples: 'Kwiatowa', 'Kwiatowa 12'")
+
+
+def validate_alpha(name):
+    if name.isalpha():
+        return True
+    else:
+        raise ValidationError("Name must contain only letters.")
+
+
+def validate_digit(number):
+    if number.isdigit():
+        return True
+    else:
+        raise ValidationError("Number must contain only digits.")
+
+
+def validate_phone_numer(phone_number):
+    if phone_number.isdigit() and len(phone_number) == 9:
+        return True
+    else:
+        raise ValidationError("Phone number must contain only digits and "
+                              "must be 9 digits long.")
+
 
 SIZE = (
     ("S", "S"),
@@ -14,27 +78,30 @@ class CustomerUser(AbstractUser, PermissionsMixin):
     """
         Custom user model for a jewelry store.
     """
+    username = None
     email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=255, null=True)
-    last_name = models.CharField(max_length=255, null=True)
-    phone_number = models.CharField(max_length=9, unique=True, null=True)
-    street = models.CharField(max_length=255, null=True)
-    house_number = models.CharField(max_length=10, null=True)
-    apartment_number = models.CharField(max_length=10, blank=True, null=True)
-    city = models.CharField(max_length=100, null=True)
-    postal_code = models.CharField(max_length=6, null=True)
-    country = models.CharField(max_length=100, null=True, default="Poland")
+    first_name = models.CharField(max_length=255, null=True,
+                                  validators=[validate_alpha])
+    last_name = models.CharField(max_length=255, null=True,
+                                 validators=[validate_alpha])
+    phone_number = models.CharField(max_length=9, unique=True, null=True,
+                                    validators=[validate_phone_numer])
+    street = models.CharField(max_length=255, null=True,
+                              validators=[validate_street_name])
+    house_number = models.CharField(max_length=10, null=True,
+                                    validators=[validate_house_number])
+    apartment_number = models.CharField(max_length=10, blank=True, null=True,
+                                        validators=[validate_digit])
+    city = models.CharField(max_length=100, null=True,
+                            validators=[validate_alpha])
+    postal_code = models.CharField(max_length=6, null=True,
+                                   validators=[validate_postal_code])
+    country = models.CharField(max_length=100, null=True, default="Poland",
+                               validators=[validate_alpha])
 
     USERNAME_FIELD = "email"
 
-    REQUIRED_FIELDS = ["username"]
-
-    is_staff = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        if not self.username:
-            self.username = str(uuid.uuid4())[:30]  # Generuje unikalne ID
-        super().save(*args, **kwargs)
+    REQUIRED_FIELDS = []
 
     @property
     def full_street(self):
@@ -60,7 +127,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(null=True)
     description = models.TextField()
-    size = models.CharField(choices=SIZE,max_length=1, blank=True, null=True)
+    size = models.CharField(choices=SIZE, max_length=1, blank=True, null=True)
 
     def __str__(self):
         return self.name + " " + str(self.price)
